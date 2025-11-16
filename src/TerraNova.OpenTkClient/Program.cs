d5cae37a7661022b1bc265ca15567dcde35d9d6f
Ben <ahrensben@gmail.com>
2025-11-15 19:18:41 -0500

    Initial desktop client

--- src/TerraNova.OpenTkClient/Program.cs
+++ src/TerraNova.OpenTkClient/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace TerraNova.OpenTkClient;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var gameWindowSettings = GameWindowSettings.Default;
+        gameWindowSettings.UpdateFrequency = 60.0;
+
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            ClientSize = new Vector2i(800, 600),
+            Title = "Terra Nova",
+            Flags = ContextFlags.ForwardCompatible,
+            APIVersion = new Version(3, 3),
+            Profile = ContextProfile.Core,
+            StartVisible = true,
+            StartFocused = true,
+            WindowBorder = WindowBorder.Resizable,
+        };
+
+        using (var window = new TerraNovaWindow(gameWindowSettings, nativeWindowSettings))
+        {
+            window.Run();
+        }
+    }
+}
---
+++ src/TerraNova.OpenTkClient/TerraNovaWindow.cs
@@ -0,0 +1,52 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace TerraNova.OpenTkClient;
+
+public class TerraNovaWindow : GameWindow
+{
+    public TerraNovaWindow(
+        GameWindowSettings gameWindowSettings,
+        NativeWindowSettings nativeWindowSettings
+    )
+        : base(gameWindowSettings, nativeWindowSettings) { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.4f, 0.8f, 1.0f);
+
+        GL.Enable(EnableCap.DepthTest);
+        GL.DepthFunc(DepthFunction.Lequal);
+
+        GL.Enable(EnableCap.CullFace);
+        GL.CullFace(TriangleFace.Back);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+    }
+}
