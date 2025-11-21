585f8d1ca42a23dae38cc54696d8738be4f68645
Ben <ahrensben@gmail.com>
2025-11-21 17:45:59 -0500

    Initial IGame interface

---
+++ src/TerraNova.Client/IGame.cs
@@ -0,0 +1,14 @@
+﻿namespace TerraNova.Client;
+
+public interface IGame
+{
+    void Load(Viewport viewport);
+
+    void Update(double deltaTime);
+
+    void Render();
+
+    void Unload();
+
+    void Resize(Viewport viewport);
+}
