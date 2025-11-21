00181811dfd50796984ba6cd012bb9e53f7eab16
Ben <ahrensben@gmail.com>
2025-11-21 18:16:09 -0500

    Use lowercase

--- src/TerraNova.Client/Viewport.cs
+++ src/TerraNova.Client/Viewport.cs
@@ -2,5 +2,5 @@
 
 public record Viewport(int X, int Y, int Width, int Height)
 {
-    public static Viewport Fullscreen(int Width, int Height) => new Viewport(0, 0, Width, Height);
+    public static Viewport Fullscreen(int width, int height) => new Viewport(0, 0, width, height);
 }
