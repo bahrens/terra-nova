3a03163e3db8c6e78b5c4c9e2022f2083f06ea69
Ben <ahrensben@gmail.com>
2025-11-21 19:09:44 -0500

    Add validation

--- src/TerraNova.Client/Viewport.cs
+++ src/TerraNova.Client/Viewport.cs
@@ -2,5 +2,18 @@
 
 public record Viewport(int X, int Y, int Width, int Height)
 {
+    public Viewport(int width, int height) : this(0, 0, width, height)
+    {
+        if (width < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(width), "Width must be non-negative.");
+        }
+
+        if (height < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(height), "Height must be non-negative.");
+        }
+    }
+
     public static Viewport Fullscreen(int width, int height) => new Viewport(0, 0, width, height);
 }
