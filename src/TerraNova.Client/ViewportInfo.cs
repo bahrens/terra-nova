5573735b4f173d97d90fa70e1dc6d1ad88bd27c3
Ben <ahrensben@gmail.com>
2025-11-24 18:26:51 -0500

    Fix formatting

--- src/TerraNova.Client/ViewportInfo.cs
+++ src/TerraNova.Client/ViewportInfo.cs
@@ -1,14 +1,14 @@
 ﻿namespace TerraNova.Client;
 
 public readonly struct ViewportInfo
-  {
-      public int Width { get; init; }
-      public int Height { get; init; }
-      public float AspectRatio => Width / (float)Height;
+{
+    public int Width { get; init; }
+    public int Height { get; init; }
+    public float AspectRatio => Width / (float)Height;
 
-      public ViewportInfo(int width, int height)
-      {
-          Width = width;
-          Height = height;
-      }
-  }
+    public ViewportInfo(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
